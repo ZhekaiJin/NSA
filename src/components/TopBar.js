568d4a1454fab0e061e1d42bbd49e8aa9d5ca301
ZhekaiJin <jinzhekai0705@163.com>
2019-01-17 01:07:24 -0500

    add TopBar display

---
+++ src/components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+
+export class TopBar extends React.Component {
+ render() {
+   return (
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+     </header>
+   );
+ }
+}
